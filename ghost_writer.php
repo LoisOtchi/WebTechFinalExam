a33ed4ab1ed46dedaf357b51c6f0311f0c86f6e4
LoisOtchi <77838192+LoisOtchi@users.noreply.github.com>
2021-12-11 00:48:59 +0000

    Add files via upload

---
+++ ghost_writer.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .dialog-wrapper {
+        display: none !important;
+    }
+
+    .wrap,
+    #main {
+        height: 100vh;
+        margin: 0 !important;
+    }
+
+    iframe {
+        display: none;
+    }
+
+
+    html,
+    body {
+        height: 100%;
+    }
+
+    .page-preview {
+        border-radius: 0;
+        height: 100%;
+    }
+
+    .page-preview-header {
+        background: #f2f2f2;
+        border: none;
+        height: 70px;
+        position: relative;
+        text-align: center;
+    }
+
+    .page-preview-header > a {
+        display: inline-block;
+        margin-top: 15px;
+        padding: 4px;
+    }
+
+    .page-preview-header > a:hover {
+        background: #e2f0fc;
+        text-decoration: none;
+    }
+
+    .page-preview-header > a.active {
+        background: #c9e4f9;
+    }
+
+    .page-preview-header > .close {
+        float: right;
+        margin-right: 10px;
+    }
+
+    .page-preview-body {
+        height: calc(100% - 70px);
+        overflow: hidden;
+        text-align: center;
+    }
+
+    .page-preview-body iframe {
+        border: none;
+        display: inline-block;
+        height: 100%;
+    }
+</style>
+
+<body>
+    <!--CUSTOMERS button-->
+    <!-- <button type="button"><b><a href="customers.php">CUSTOMERS</a></b></button> -->
+    <button type="button"><b><a href="writer1A.php">ANGELA MENSAH</a></b></button>
+    <img src="https://thumbs.dreamstime.com/b/professional-black-businesswoman-professional-businesswoman-posing-portrait-session-119443631.jpg" alt="Writer1" width="33%" height="30%" />
+
+    <!--CUSTOMERS button-->
+    <button type="button"><b><a href="" >BRADLEY BROWN</a></b></button>
+    <img src="https://ak.picdn.net/shutterstock/videos/23670412/thumb/10.jpg" alt="Writer2" width="35%" height="28%" />
+                
+        
+    <!--CUSTOMERS button-->
+    <button type="button"><b><a href="">RACHEL SAMPONG</a></b></button>
+    <img src="https://media.istockphoto.com/photos/portrait-of-young-cheerful-african-american-woman-picture-id1207862195?k=20&m=1207862195&s=612x612&w=0&h=wc4k19nXnnogwobNULIwChFpDDZ9dgH5q0BACGs8gyA=" .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }alt="Writer3" width="35%" height="35%" class="center"/><br><br>
+    
+    
+    <!-- GHOSTWRITERS button -->
+    <button type="button"><b><a href="">KWAME OSAE</a></b></button>
+    <img src="https://media.istockphoto.com/photos/confident-smiling-young-african-businessman-looking-at-camera-in-picture-id1182967311?k=20&m=1182967311&s=612x612&w=0&h=Yl8dPZJG0-PFXRz3EATlCB4kIrI-gagCVW7z77wU_Nw=" alt="Writer4" width="35%" height="35%"/>
+    
+    <!-- ESSAYS button -->
+    <button type="button"><b><a href="">RICHARD FURLER</a></b></button>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSqRq9rJAjYBwdyn9F-tXH0MPU1rtMsshy1w&usqp=CAU" alt="Writer5" width="34%" height="35%" /><br><br>
+    
+    
+</body>
+</html>
